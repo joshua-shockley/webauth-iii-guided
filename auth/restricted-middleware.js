439c2aec6e0bec74d753e4c29ecd55db07400b2d
Joshua-Shockley <thelumbashockley@gmail.com>
2019-10-16 12:23:09 -0500

    its working...

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,24 @@
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
+// const Users = require('../users/users-model.js');
 
-const Users = require('../users/users-model.js');
-
+const jwt = require('jsonwebtoken');
+const secret = require('../config/secrets.js');
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+    const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
-  } else {
-    res.status(400).json({ message: 'No credentials provided' });
-  }
+    if (token) {
+        jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+            if (err) {
+                //foulplay
+                res.status(401).json({ message: 'no way' })
+            } else {
+                //token good
+                req.username = decodedToken.username;
+                next();
+            }
+        })
+    } else {
+        res.status(400).json({ message: 'no token provided' });
+
+    }
 };
